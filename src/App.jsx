1a000516bb9b887119ecdca70001644a6068b851
Zahid Nawaz <151016187+ShahidNawazBlockchain@users.noreply.github.com>
2024-03-06 10:17:42 -0800

    button comonent is completed. i create dynamic button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css'
-
-function App() {
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <Button className="mt-20">
+    Some </Button>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-20" href="#login">
+        Something
+        </Button>
+      </div>
+      <h1> <ButtonGradient /> </h1>
     </>
   )
 }
-
 export default App
